e1d169b3089ecc2a34c41b671fe7903eb5511cbe
novaardiansyah <novaardiansyah78@gmail.com>
2021-07-01 16:08:45 +0700

    prepare data for requests and also axios

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,15 @@
 import React from 'react'
+// static components
+import {requests} from './static/requests'
 
 export default function Home() {
   return (
-    <div>Hello World!</div>  
+    <div className="container">
+      <div className="row">
+        <div className="col-md-10 col-lg-8">
+          <div>hello world</div>
+        </div>
+      </div>
+    </div>
   )
 }
