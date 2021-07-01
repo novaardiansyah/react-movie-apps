4163bafbdd1fda079c7bdaee8ab9a8a2dce53392
novaardiansyah <novaardiansyah78@gmail.com>
2021-07-01 12:44:45 +0700

    setup project

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './router/App'
+
+import './assets/scss/app.scss'
+
+ReactDOM.render(<App />, document.getElementById('root'))
