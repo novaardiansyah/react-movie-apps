1f6dc20afb930421ec00cddb07a2f9c0d43405f9
novaardiansyah <novaardiansyah78@gmail.com>
2021-07-01 17:50:46 +0700

    display poster to webpage

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,22 +6,21 @@ import MovieList from './movies/MovieList'
 
 export default function Home() {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-10 col-lg-8">
-          <MovieList
-            title="NETFLIX ORIGINALS"
-            fetchUrl={requests.netflixOriginals}
-          />
-          <MovieList title="Trending" fetchUrl={requests.trending} />
-          <MovieList title="Top Rated" fetchUrl={requests.topRated} />
-          <MovieList title="Action Movies" fetchUrl={requests.actionMovies} />
-          <MovieList title="Comedy Movies" fetchUrl={requests.comedyMovies} />
-          <MovieList title="Horror Movies" fetchUrl={requests.horrorMovies} />
-          <MovieList title="Romance Movies" fetchUrl={requests.romanceMovies} />
-          <MovieList title="Documentaries" fetchUrl={requests.documentaries} />
-        </div>
-      </div>
+    <div className="container my-3">
+      <MovieList
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.netflixOriginals}
+      />
+
+      {/*
+        <MovieList title="Trending" fetchUrl={requests.trending} />
+        <MovieList title="Top Rated" fetchUrl={requests.topRated} />
+        <MovieList title="Action Movies" fetchUrl={requests.actionMovies} />
+        <MovieList title="Comedy Movies" fetchUrl={requests.comedyMovies} />
+        <MovieList title="Horror Movies" fetchUrl={requests.horrorMovies} />
+        <MovieList title="Romance Movies" fetchUrl={requests.romanceMovies} />
+        <MovieList title="Documentaries" fetchUrl={requests.documentaries} />
+      */}
     </div>
   )
 }
--- src/components/movies/MovieList.js
+++ src/components/movies/MovieList.js
@@ -1,20 +1,40 @@
 import React, { useState, useEffect } from 'react'
+// style
+import '../../assets/scss/components/movies/MovieList.scss'
+
 // static
 import { axiosInstance } from '../static/axios'
 
 export default function MovieList({ title, fetchUrl }) {
   const [movies, setMovies] = useState([])
-  
+
   useEffect(() => {
     async function getMovies() {
       const request = await axiosInstance.get(fetchUrl)
-      console.log(request.data.results)
+
+      return setMovies(request.data.results)
     }
-    
     getMovies()
-  })
-  
+  }, [fetchUrl])
+
+  const baseImageUrl = 'https://image.tmdb.org/t/p/original'
+
   return (
-    <div>{ title }</div>
+    <div className="row justify-content-center">
+      <div className="col-md-10 col-lg-8">
+        <h3 className="movie__title">{title}</h3>
+      </div>
+
+      <div className="col-md-10 col-lg-8 d-flex flex-row overflow-scroll">
+        {movies.map((movie) => (
+          <img
+            src={`${baseImageUrl}/${movie.poster_path}`}
+            alt={movie.name}
+            key={movie.id}
+            className="movie__poster me-1"
+          />
+        ))}
+      </div>
+    </div>
   )
 }
